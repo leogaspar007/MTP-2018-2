cf190c31a75deaebdf76aea93d97df27c947d267
leogaspar007 <42418813+leogaspar007@users.noreply.github.com>
2018-09-12 16:07:20 -0300

    Add files via upload

---
+++ P1.c
@@ -0,0 +1,49 @@
+//P1.c
+//Leonardo Assis Gaspar
+//11811EAU015
+
+#include <stdio.h>
+
+int main()
+{
+	int estado = 0, i;
+	char bits[256];
+	printf("Digite a sequencia desejada: ");
+	fflush(stdin);
+	scanf("%s", &bits);
+	for(i = 0; bits[i] != '\0'; i++)
+	{
+		for(i = 0; bits[i] != '\0'; i++)
+		{
+			if(estado == 0 && bits[i] == '0');
+			else if(estado == 0 && bits[i] == '1')
+				{
+					estado = 1;
+				}
+			else if(estado == 1 && bits[i] == '0')
+				{
+					estado = 2;	
+				}
+			else if(estado == 1 && bits[i] == '1')
+				{
+					estado = 0;
+				}
+			else if(estado == 2 && bits[i] == '0')
+				{
+					estado = 1;	
+				}
+			else if(estado == 2 && bits[i] == '1');
+		}
+	}
+	if (estado == 0)
+	{
+		printf("O estado final foi: %i\n", estado);
+		printf("%s e uma sequencia multipla de 3.", bits);
+	}
+	else
+	{
+		printf("O estado final foi: %i\n", estado);
+		printf("%s nao e uma sequencia multipla de 3.", bits);
+	}
+	return 0;
+}
